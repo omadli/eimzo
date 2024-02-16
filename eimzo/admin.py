06a62accc9483dc1908357658927e18940c7a815
Omadli Yigit <76625693+omadli@users.noreply.github.com>
2024-02-16 18:14:42 +0500

    some updates

--- eimzo/admin.py
+++ eimzo/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from django.http import HttpRequest, HttpResponse
-from django.utils.html import format_html
 from django.db.models import QuerySet
+from django.utils.html import format_html
+from django.http import HttpRequest, HttpResponse
+from django.core.exceptions import PermissionDenied
 
 from eimzo.models import Key
-from eimzo.excel import download_as_excel
+from eimzo.excel import download_as_excel_full, download_as_excel_short
+
 
 dl_icon = """<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16"> 
   <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
@@ -12,14 +14,25 @@ dl_icon = """<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill
 </svg>"""  # noqa: E501
 
 
-@admin.action(description="Download as excel")
-def download_keys_as_excel(modeladmin: admin.ModelAdmin, request: HttpRequest, queryset: QuerySet[Key]) -> HttpResponse:  # noqa: E501
-    output = download_as_excel(queryset)
+@admin.action(description="Download as excel full")
+def action_download_as_excel_full(modeladmin: admin.ModelAdmin, request: HttpRequest, queryset: QuerySet[Key]) -> HttpResponse:  # noqa: E501
+    output = download_as_excel_full(queryset)
     response = HttpResponse(
         output.read(),
         content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
     )
     response['Content-Disposition'] = 'attachment; filename=certificates.xlsx'
+    return response
+
+
+@admin.action(description="Download as excel short")
+def action_download_as_excel_short(modeladmin: admin.ModelAdmin, request: HttpRequest, queryset: QuerySet[Key]) -> HttpResponse:  # noqa: E501
+    output = download_as_excel_short(queryset)
+    response = HttpResponse(
+        output.read(),
+        content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    )
+    response['Content-Disposition'] = 'attachment; filename=Kalitlar.xlsx'
     return response
     
     
@@ -28,7 +41,7 @@ class KeyAdmin(admin.ModelAdmin):
     list_display_links = ('id', 'serial_number',)
     list_filter = ('type', 'valid_to',)
     search_fields = ("organization", "stir", "uid", "full_name", "serial_number")
-    actions = (download_keys_as_excel, )
+    actions = (action_download_as_excel_short, action_download_as_excel_full)
     readonly_fields = ["serial_number", "stir", "uid", "jshshir", "city", "country", "valid_from", "valid_to"]  # noqa: E501
     
     fieldsets = [
--- eimzo/excel.py
+++ eimzo/excel.py
@@ -5,7 +5,7 @@ from django.db.models import QuerySet
 from eimzo.models import Key
 
 
-def download_as_excel(queryset: QuerySet[Key]) -> BytesIO:
+def download_as_excel_full(queryset: QuerySet[Key]) -> BytesIO:
     output = BytesIO()
     wb = xlsxwriter.Workbook(output, options={"remove_timezone": True})
     w = wb.add_worksheet("keys")
@@ -68,6 +68,60 @@ def download_as_excel(queryset: QuerySet[Key]) -> BytesIO:
         w.write(f"R{i}", key.business_category)
         w.write_datetime(f"S{i}", key.valid_from, datetime_format)
         w.write_datetime(f"T{i}", key.valid_to, datetime_format)
+    
+    w.autofit()
+    wb.close()
+    output.seek(0)
+    return output
+
+def download_as_excel_short(queryset: QuerySet[Key]) -> BytesIO:
+    # queryset = queryset.order_by("valid_to")
+    output = BytesIO()
+    wb = xlsxwriter.Workbook(output, options={"remove_timezone": True})
+    w = wb.add_worksheet("keys")
+    w.freeze_panes(1, 1)
+    bold = wb.add_format({'bold': True})
+    number_format1 = wb.add_format({'num_format': '0'})
+    number_format2 = wb.add_format({'num_format': '# ##0'})
+    datetime_format = wb.add_format({'num_format': 'dd/mm/yyyy hh:mm:ss'})
+    
+    titles = [
+        "№", # A
+        "Fayl", # B
+        "Parol", # C
+        "Turi",  # D
+        "SerialNumber", #E
+        "STIR",     # F
+        "KORXONA", # G
+        "RAHBAR STIR", # H
+        "RAHBAR",   # I
+        "JSHSHIR",    # J
+        "OLINGAN SANASI",   # K
+        "YAROQLILIK MUDDATI" # L
+    ]
+    
+    len_table = len(queryset) + 1
+    w.add_table(f"A1:L{len_table}", 
+        {
+            "header_row": True, 
+            "name": "keys",
+            "columns": [{"header": i, "header_format": bold} for i in titles]
+        }
+    )
+        
+    for i, key in enumerate(queryset, start=2):
+        w.write_number(f"A{i}", i-1)
+        w.write_string(f"B{i}", key.file.name)
+        w.write(f"C{i}", key.password, number_format2)
+        w.write_string(f"D{i}", key.KeyType(key.type).label)
+        w.write_string(f"E{i}", key.serial_number)
+        w.write_number(f"F{i}", key.stir, number_format1)
+        w.write_string(f"G{i}", key.organization.upper())
+        w.write_number(f"H{i}", key.uid, number_format1)
+        w.write_string(f"I{i}", key.full_name.upper())
+        w.write_number(f"J{i}", key.jshshir, number_format1)
+        w.write_datetime(f"K{i}", key.valid_from, datetime_format)
+        w.write_datetime(f"L{i}", key.valid_to, datetime_format)
     
     w.autofit()
     wb.close()
